3016732be85dc313e8fb9051f0c279b02c5a3db8
Nathan Randle <nrandle@dolbz.com>
2023-01-01 22:21:10 +0000

    Day 20

---
+++ day20/part1.py
@@ -0,0 +1,42 @@
+def read_input_file():
+    file = open('input.txt', 'r')
+    lines = file.readlines()
+
+    return [(int(line), False) for line in lines]
+
+
+encrypted_message = read_input_file()
+
+for _ in range(len(encrypted_message)):
+    current_index = 0
+    current_number = 0
+
+    for index, (number, already_processed) in enumerate(encrypted_message):
+        if not already_processed:
+            current_index = index
+            current_number = number
+            break
+    new_index = current_index + current_number
+
+    if new_index >= len(encrypted_message):
+        new_index = new_index % (len(encrypted_message) - 1)
+    elif new_index < 0:
+        while new_index < 0:
+            new_index = len(encrypted_message) - 1 + new_index
+
+    encrypted_message.pop(current_index)
+    encrypted_message.insert(new_index, (number, True))
+
+message_numbers_only = [number for (number, _) in encrypted_message]
+print(message_numbers_only)
+
+while message_numbers_only[0] != 0:
+    num = message_numbers_only.pop(0)
+    message_numbers_only.append(num)
+
+coord1 = message_numbers_only[1000 % len(message_numbers_only)]
+coord2 = message_numbers_only[2000 % len(message_numbers_only)]
+coord3 = message_numbers_only[3000 % len(message_numbers_only)]
+
+print(f'{coord1}, {coord2}, {coord3}')
+print(coord1 + coord2 + coord3)
